dc6a5a3cdfd6a3edcff10a66bd7bceb7cffb2354
Liseczekq <polskilis3335@gmail.com>
2021-06-19 12:29:47 +0200

    First Commit of Full Project

--- src/main/java/model/Song.java
+++ src/main/java/model/Song.java
@@ -1,2 +1,113 @@
-package model;public class Song {
+package model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Song {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String genre;
+    private String ismn;
+    private String year;
+
+    @ManyToMany
+    private String publisher;
+
+    @ManyToMany
+    private Set<Artist> artists = new HashSet<>();
+
+
+    public Song() {
+    }
+
+    public Song(String title, String genre, String ismn, String year, String publisher, Set<Artist> artists) {
+        this.title = title;
+        this.genre = genre;
+        this.ismn = ismn;
+        this.year = year;
+        this.publisher = publisher;
+        this.artists = artists;
+    }
+
+    public Song(String title, String genre, String ismn, String year, String publisher) {
+        this.title = title;
+        this.genre = genre;
+        this.ismn = ismn;
+        this.year = year;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getIsmn() {
+        return ismn;
+    }
+
+    public void setIsmn(String ismn) {
+        this.ismn = ismn;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Artist> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(Set<Artist> artists) {
+        this.artists = artists;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", ismn='" + ismn + '\'' +
+                ", year='" + year + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", artists=" + artists +
+                '}';
+    }
 }
